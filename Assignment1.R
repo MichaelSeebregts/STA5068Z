1259040294a6c4912cf7e58d688f5b8cf0e55a55
MichaelSeebregts <mjseebregts@gmail.com>
2025-10-23 00:29:29 +0200

    update

--- Assignment1.R
+++ Assignment1.R
@@ -1,5 +1,5 @@
 ### Q1 
-legendre = function(order, x)
+legendre = function(order, x, sigma = 0)
 {
   
   L = 0
@@ -89,6 +89,7 @@ legenTarget(3, 5, xSeq)
 
 
 ### Q2
+
 
 
 ###
@@ -223,7 +224,7 @@ bias_var = function(N, M, hyp, dx, sig)
   
   gBar = gBar/M
   
-  phi_x = 1/2
+  phi_x = 1/4
   bias2 = sum((gBar - underlying(xx_lat))[-Nx]^2*phi_x*dx)
   bias2
   
@@ -237,26 +238,215 @@ bias_var = function(N, M, hyp, dx, sig)
   
   both = bias2 + var
   
-  return(list("testError" = testError, "biasSquared" = bias2, "variance" = var))
+  return(list("testError" = testError, "bias" = bias2, "variance" = var, "OOSE" = both))
 }
 
 biasVarH1 = bias_var(5, 1000, 1, 1/100, 0)
 biasVarH2 = bias_var(5, 1000, 2, 1/100, 0)
 
-varyingH1 = data.frame()
+varyingH1 = data.frame("Bias" = numeric(0), "Var" = numeric(0), "OOSE" = numeric(0))
+varyingH2 = data.frame("Bias" = numeric(0), "Var" = numeric(0), "OOSE" = numeric(0))
+
+sigSeq = c(0, 0.1, 0.2, 0.3, 0.4, 0.5)
+
+for (j in 1:length(sigSeq))
+{
+  tempH1 = bias_var(5, 1000, 1, 1/100, sigSeq[j])
+  tempH2 = bias_var(5, 1000, 2, 1/100, sigSeq[j])
+  
+  tempH1Res = c(tempH1$bias, tempH1$variance, tempH1$testError)
+  tempH2Res = c(tempH2$bias, tempH2$variance, tempH2$testError)
+  
+  varyingH1 = rbind(varyingH1, tempH1Res)
+  varyingH2 = rbind(varyingH2, tempH2Res)
+}
+
+colnames(varyingH1) = c("Bias", "Variance", "OOSE")
+colnames(varyingH2) = c("Bias", "Variance", "OOSE")
+
+varyingH1 = cbind(sigSeq, varyingH1)
+varyingH2 = cbind(sigSeq, varyingH2)
+
+plot(varyingH1$sigSeq, varyingH1$Bias, type = "l", ylim = c(min(varyingH1$Bias, varyingH2$Bias), max(varyingH1$Bias, varyingH2$Bias)), 
+     xlab = "Sigma", ylab = "Bias")
+lines(varyingH2$sigSeq, varyingH2$Bias, col = "red")
+
+plot(varyingH1$sigSeq, varyingH1$Variance, type = "l", ylim = c(min(varyingH1$Variance, varyingH2$Variance), max(varyingH1$Variance, varyingH2$Variance)), 
+     xlab = "Sigma", ylab = "Variance")
+lines(varyingH2$sigSeq, varyingH2$Variance, col = "red")
+
+plot(varyingH1$sigSeq, varyingH1$OOSE, type = "l", ylim = c(min(varyingH1$OOSE, varyingH2$OOSE), max(varyingH1$OOSE, varyingH2$OOSE)), 
+     xlab = "Sigma", ylab = "Out of Sample Error")
+lines(varyingH2$sigSeq, varyingH2$OOSE, col = "red")
 
 ###
 
 
 
 ### Q4 
+
+library(colorspace)
+
+color.gradient = function(x, colors=c('cyan','orange','magenta'), colsteps=50)
+{
+  colpal = colorRampPalette(colors)
+  return( colpal(colsteps)[ findInterval(x, seq(min(x),max(x), length=colsteps)) ] )
+}
+
 dat = read.table("Collider_Data_2022.txt", h = TRUE,stringsAsFactors =TRUE)
 
 head(dat)
 
-max.col(dat[, 3:5])
-
 plot(dat$X1, dat$X2, col = max.col(dat[, 3:5]), type = "p", lwd = 4)
+
+X = cbind(dat[, 1:2])
+Y = dat[, 3:5]
+
+softMax = function(x)
+{
+  denom = sum(exp(x))
+  softVec = rep(0, length.out = length(x))
+  for(i in 1:length(x))
+  {
+    softVec[i] = exp(x[i])/denom
+  }
+  return(softVec)
+}
+
+softMaxMatrix = function(X)
+{
+  softMat = apply(X, 2, softMax)
+  return(softMat)
+}
+
+
+neural_net = function(X,Y,theta,m,nu)
+{
+  # Infer dimensions:
+  N = dim(X)[1]
+  p = dim(X)[2]
+  q = dim(Y)[2]
+  
+  d = c(p,m,q) 
+  
+  # Populate weight and bias matrices:
+  index = 1:(d[1]*d[2])
+  W1    = matrix(theta[index],d[1],d[2])
+  index = max(index)+(1:(d[2]*d[3]))
+  W2    = matrix(theta[index],d[2],d[3])
+  index = max(index)+(1:d[2])
+  b1    = matrix(theta[index],d[2],1)
+  index =max(index)+(1:d[3])
+  b2    = matrix(theta[index],d[3],1)
+  
+  ones = matrix(1,N,1)
+  # Evaluate the updating equation in matrix form
+  #for(i in 1:N)
+  #{
+  A0 = t(X)
+  A1 = tanh(t(W1)%*%A0+b1%*%t(ones))
+  
+  #print(head(t(W2)%*%A1+b2%*%t(ones)))
+  A2 = softMaxMatrix(t(W2)%*%A1+b2%*%t(ones))
+  #print(head(A2))
+  #}
+  yHat = A2
+  # print(dim(yHat))
+  # print(A2)
+  
+  error = rep(NA,N)    #-(Y*log(pi_hat)+(1-Y)*log(1-pi_hat))
+  logA2 = log(A2)
+  crossEnt = t(Y)*logA2
+  #print(head(crossEnt))
+  error = apply(crossEnt, 2, sum)
+  # Evaluate an appropriate objective function and return some predictions:
+  E1 = (-1)*sum(error)/N
+  E2 = E1+nu/N*(sum(W1^2)+sum(W2^2))
+  # Return a list of relevant objects:
+  return(list(A2 = A2,A1 = A1, E1 = E1, E2 = E2))
+}
+m   = 10
+p     = dim(X)[2]
+q     = dim(Y)[2]
+npars = p*m+m*q+m+q
+theta_rand = runif(npars,-1,+1)
+res = neural_net(X,Y,theta_rand,m,0)
+
+set.seed(2022)
+
+M  = 100
+x1 = seq(-3,3,length = M)
+x2 = seq(-3,3,length = M)
+xx1 = rep(x1,M) 
+xx2 = rep(x2, each = M)
+points(xx2~xx1, pch = 16,cex = 0.75)
+abline(v = x1,h = x2,lty =2)
+
+XX = cbind(xx1,xx2)
+YY = matrix(1,M^2,3)
+
+
+N      = dim(X)[1]
+set    = sample(1:N,0.5*N,replace = FALSE)
+Xtrain = X[set,,drop = FALSE] 
+Ytrain = Y[set,,drop = FALSE] 
+Xval   = X[-set,,drop = FALSE]
+Yval   = Y[-set,,drop = FALSE]
+
+
+# nu = 5
+obj_pen = function(pars)
+{
+  res = neural_net(Xtrain,Ytrain,pars,m,nu)
+  return(res$E2)
+}
+# obj_pen(theta_rand)
+
+
+n_nus     = 10
+nu_seq    = exp(seq(-5,0,length = n_nus))
+val_error = rep(NA,n_nus)
+for(i in 1:n_nus)
+{
+  nu  = nu_seq[i]
+  # Fit the neural network using a standard optimizer in R:
+  res_opt = nlm(obj_pen,theta_rand, iterlim = 1000)
+  
+  res_val      = neural_net(Xval,Yval,res_opt$estimate,m,0)
+  val_error[i] = res_val$E1
+  
+  # Draw a response curve over the 2D input space to see
+  # what pattern the neural network predicts
+  
+  # res_fitted = neural_net(XX,YY,res_opt$estimate,m,0)
+  # 
+  # plot(XX[,2]~XX[,1], pch = 16, col = color.gradient(max.col(t(res_fitted$A2))))
+  # points(X$X2~X$X1, col = max.col(Y), pch = 16)
+  # 
+  # print(paste0('Validation run ', i,'| nu = ',round(nu,5)))
+}
+
+plot(val_error~nu_seq,type = 'b',pch = 16,cex = 0.75)
+
+wh = which.min(val_error)
+
+nu_final = nu_seq[wh]
+nu_final_conservative = nu_seq[wh+1]
+abline(v = nu_final)
+abline(v = nu_final_conservative)
+obj_final = function(pars)
+{
+  res = neural_net(X,Y,pars,m,nu_final_conservative)
+  return(res$E2)
+}
+res_opt_final = nlm(obj_final,theta_rand, iterlim = 1000)
+
+
+res_fitted = neural_net(XX,YY,res_opt_final$estimate,m,0)
+
+plot(XX[,2]~XX[,1], pch = 16, col = color.gradient(max.col(t(res_fitted$A2))))
+points(X$X2~X$X1, col = max.col(Y), pch = 16)
+
 
 ####
 
